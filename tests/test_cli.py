d900f6c0271a21f2cbd37670f178da05c446e351
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-12-29 10:01:18 -0500

    Initially setup.py file

---
+++ tests/test_cli.py
@@ -0,0 +1,31 @@
+import pytest
+
+from hr import cli
+
+@pytest.fixture()
+def parser():
+    return cli.create_parser()
+
+def test_parser_fails_without_arguments(parser):
+    """
+    Without a path the parser should exit with an error.
+    """
+
+def test_parser_succeeds_with_a_path(parser):
+    """
+    With path the parser should exit with a error.
+    """
+    args = parser.parse_args(['/some/path'])
+    assert args.path == '/some/path'
+
+def test_parser_export_flag(parser):
+    """
+    The `export` value should default to False, but set
+    to True when passed to the parser.
+    """
+
+    args = parser.parser_args(['/some/path'])
+    assert args.export == False
+
+    args = parser.parser_args(['--export', '/some/path'])
+    assert args.export == True
